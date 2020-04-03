b3b99d0ca37b016c4941dbf3acf1f92cbe2cf731
ljurak <lucas.jurak@gmail.com>
2020-04-03 13:57:06 +0200

    Add enum for types of matrix transposition

---
+++ src/main/java/matrix/TranspositionType.java
@@ -0,0 +1,28 @@
+package matrix;
+
+public enum TranspositionType {
+
+    MAIN_DIAGONAL(1),
+    SIDE_DIAGONAL(2),
+    VERTICAL_LINE(3),
+    HORIZONTl_LINE(4);
+
+    private int type;
+
+    private TranspositionType(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public static TranspositionType valueOfType(int value) {
+        for (TranspositionType type : values()) {
+            if (type.getType() == value) {
+                return type;
+            }
+        }
+        return null;
+    }
+}
