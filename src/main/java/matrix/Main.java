b2f648431aa4308f928e62f520309554b59a70f0
ljurak <lucas.jurak@gmail.com>
2020-04-03 13:10:46 +0200

    Add menu and matrix multiplication

--- src/main/java/matrix/Main.java
+++ src/main/java/matrix/Main.java
@@ -1,4 +1,4 @@
-package search;
+package matrix;
 
 import java.util.Scanner;
 
@@ -6,89 +6,95 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        MatrixProcessor processor = new MatrixProcessor();
 
-        int[][] firstMatrix = readMatrix(scanner);
-        int[][] secondMatrix = readMatrix(scanner);
-
-        if (firstMatrix.length != secondMatrix.length || firstMatrix[0].length != secondMatrix[0].length) {
-            System.out.println("ERROR");
-        } else {
-            printMatrix(addMatrices(firstMatrix, secondMatrix));
-        }
-
-        /*showMenu();
+        showMenu();
         int option;
+        double[][] firstMatrix;
+        double[][] secondMatrix;
         while ((option = Integer.parseInt(scanner.nextLine())) != 0) {
             System.out.println();
             switch (option) {
                 case 1:
-                    System.out.println("Select a matching strategy: ALL, ANY, NONE");
-                    System.out.println("Enter a name or email to search all suitable people.");
-                    String query = scanner.nextLine();
-                    System.out.println();
+                    firstMatrix = readMatrix(scanner);
+                    secondMatrix = readMatrix(scanner);
+                    processAddition(firstMatrix, secondMatrix, processor);
                     break;
                 case 2:
+                    firstMatrix = readMatrix(scanner);
+                    System.out.print("Enter a number: ");
+                    int number = Integer.parseInt(scanner.nextLine());
+                    processMultiplication(firstMatrix, number, processor);
+                    break;
+                case 3:
+                    firstMatrix = readMatrix(scanner);
+                    secondMatrix = readMatrix(scanner);
+                    processMultiplication(firstMatrix, secondMatrix, processor);
                     break;
                 default:
                     System.out.println("Incorrect option! Try again.\n");
                     break;
             }
             showMenu();
-        }*/
+        }
     }
 
-    private static int[][] readMatrix(Scanner scanner) {
+    private static void showMenu() {
+        System.out.println("1. Add matrices");
+        System.out.println("2. Multiply matrix by a number");
+        System.out.println("3. Multiply matrices");
+        System.out.println("0. Exit");
+        System.out.print("Your choice: ");
+    }
+
+    private static void processAddition(double[][] first, double[][] second, MatrixProcessor processor) {
+        if (first.length != second.length || first[0].length != second[0].length) {
+            System.out.println("ERROR\n");
+        } else {
+            System.out.println("The addition result is:");
+            printMatrix(processor.addMatrices(first, second));
+            System.out.println();
+        }
+    }
+
+    private static void processMultiplication(double[][] first, double[][] second, MatrixProcessor processor) {
+        if (first[0].length != second.length) {
+            System.out.println("ERROR\n");
+        } else {
+            System.out.println("The multiplication result is:");
+            printMatrix(processor.multiplyMatrices(first, second));
+            System.out.println();
+        }
+    }
+
+    private static void processMultiplication(double[][] first, int number, MatrixProcessor processor) {
+        System.out.println("The multiplication result is:");
+        printMatrix(processor.multiplyMatrixByNumber(first, number));
+        System.out.println();
+    }
+
+    private static double[][] readMatrix(Scanner scanner) {
+        System.out.print("Enter size of matrix: ");
         int rows = scanner.nextInt();
         int cols = scanner.nextInt();
         scanner.nextLine();
-        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter matrix:");
+        double[][] matrix = new double[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                matrix[i][j] = scanner.nextInt();
+                matrix[i][j] = scanner.nextDouble();
             }
             scanner.nextLine();
         }
         return matrix;
     }
 
-    private static int[][] addMatrices(int[][] first, int[][] second) {
-        if (first.length != second.length || first[0].length != second[0].length) {
-            throw new IllegalArgumentException("Cannot add matrices of different dimensions");
-        }
-
-        int rows = first.length;
-        int cols = first[0].length;
-        int[][] result = new int[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result[i][j] = first[i][j] + second[i][j];
-            }
-        }
-        return result;
-    }
-
-    private static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + " ");
+    private static void printMatrix(double[][] matrix) {
+        for (double[] row : matrix) {
+            for (double value : row) {
+                System.out.printf("%.2f ", value);
             }
             System.out.println();
         }
-    }
-
-    private static void showMenu() {
-        System.out.println("=== MENU ===");
-        System.out.println("1. Find a person");
-        System.out.println("2. Print all people");
-        System.out.println("0. Exit");
-    }
-
-    private static <T> void printData(T[] data, String title) {
-        System.out.println(title);
-        for (T el : data) {
-            System.out.println(el);
-        }
-        System.out.println();
     }
 }
