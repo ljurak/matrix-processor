8d525ef06e1ad411225d330c8a05039b5fdf614e
ljurak <lucas.jurak@gmail.com>
2020-04-03 14:33:33 +0200

    Add operations for matrix transposition

--- src/main/java/matrix/MatrixProcessor.java
+++ src/main/java/matrix/MatrixProcessor.java
@@ -49,7 +49,73 @@ public class MatrixProcessor {
                 }
             }
         }
+        return result;
+    }
 
+    public double[][] transposeMatrix(double[][] matrix, TranspositionType type) {
+        switch (type) {
+            case MAIN_DIAGONAL:
+                return transposeMainDiagonal(matrix);
+            case SIDE_DIAGONAL:
+                return transposeSideDiagonal(matrix);
+            case HORIZONTAL_LINE:
+                return transposeHorizontalLine(matrix);
+            case VERTICAL_LINE:
+                return transposeVerticalLine(matrix);
+            default:
+                throw new IllegalArgumentException("Transposition type cannot be null");
+        }
+    }
+
+    private double[][] transposeMainDiagonal(double[][] matrix) {
+        int rows = matrix[0].length;
+        int cols = matrix.length;
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix[j][i];
+            }
+        }
+        return result;
+    }
+
+    private double[][] transposeSideDiagonal(double[][] matrix) {
+        int rows = matrix[0].length;
+        int cols = matrix.length;
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix[cols - 1 - j][rows - 1 - i];
+            }
+        }
+        return result;
+    }
+
+    private double[][] transposeVerticalLine(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix[i][cols - 1 - j];
+            }
+        }
+        return result;
+    }
+
+    private double[][] transposeHorizontalLine(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix[rows - 1 - i][j];
+            }
+        }
         return result;
     }
 }
